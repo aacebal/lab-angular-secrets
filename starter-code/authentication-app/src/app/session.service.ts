b84bdf0c480517c737bcfd724ad1eb6c7283c116
Adel Acebal <adelacebal@yahoo.com>
2017-07-04 14:26:33 -0400

    first commit

--- starter-code/authentication-app/src/app/session.service.ts
+++ starter-code/authentication-app/src/app/session.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class SessionService {
-  constructor() { }
+  constructor( private http: Http) { }
+
+  handleError(e) {
+    return Observable.throw(e.json().message);
+  }
+
+  singup(user) {
+    return this.http.post('/signup', user).
+      map((res) => res.json()).
+        catch(this.handleError)
+  }
+
 }
